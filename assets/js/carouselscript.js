04487332c2aec66106d57bd23884c03d65b28737
Raben1997 <raben.haldar.1993@gmail.com>
2025-01-12 01:47:34 +0530

    Upto About Section

--- assets/js/carouselscript.js
+++ assets/js/carouselscript.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-    $('.navbar-toggler').click(function(){
-        $('.navbar-toggler').toggleClass('showtoggle');
-    });	
-});	
-$(document).ready(function(){
-    $('.dropdown-menu .dropdown-toggle').on("click", function(e){
-        $(this).next('.dropdown-menu').toggle();
-        e.stopPropagation();
-        e.preventDefault();
-    });
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
+    $(".navbar-toggler").toggleClass("showtoggle");
+  });
 });
-$(window).scroll(function() {    
+$(document).ready(function () {
+  $(".dropdown-menu .dropdown-toggle").on("click", function (e) {
+    $(this).next(".dropdown-menu").toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+$(window).scroll(function () {
   var scroll = $(window).scrollTop();
   if (scroll >= 30) {
-      $(".header_sec").addClass("fixed");
+    $(".header_sec").addClass("fixed");
   } else {
-      $(".header_sec").removeClass("fixed");
+    $(".header_sec").removeClass("fixed");
   }
 });
 
